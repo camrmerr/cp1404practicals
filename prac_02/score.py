3a2c0102cd16890f0d3a3e270217a5c2ce227281
camrmerr <Kn3z9yMKf8qzHdu>
2022-09-24 15:48:07 +1000

    Refactored to use function and add random feature

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,13 +1,30 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Program to determine score status
 """
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+from random import randint
+
+
+def main():
+    score = float(input("Enter score: "))
+    grade = grade_score(score)
+    print(f"Result: {grade}")
+    random_score = randint(0, 100)
+    grade = grade_score(random_score)
+    print(f"Result: {grade}")
+
+
+def grade_score(score):
+    """Convert score to grade"""
+    if score < 0 or score > 100:
+        result = "Invalid Score"
+    elif score < 50:
+        result = "Bad"
+    elif score < 90:
+        result = "Passable"
+    else:
+        result = "Excellent"
+    return result
+
+
+main()
