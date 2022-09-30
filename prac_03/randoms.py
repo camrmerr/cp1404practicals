c68f0f0a7bc13e1bad6f0f839f00bff878a0c805
camrmerr <Kn3z9yMKf8qzHdu>
2022-09-30 19:42:24 +1000

    Performed practical task for randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,12 @@
+"""Answers for random exercise"""
+# line 1 Saw a random number, lowest was 5 largest was 20
+
+# line 2 Saw random odd number, lowest is 3 largest was 9
+# line 2 could not have produced a 4
+
+# line 3 Saw random number with 16 significant figures
+# line 3 could have been between 2.500000000000001 and 5.499999999999999
+
+import random
+
+print(random.randint(1, 100))
