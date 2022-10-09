d70076534b4ec4fa77d4afe1fbb640edf1619569
camrmerr <Kn3z9yMKf8qzHdu>
2022-10-09 10:16:31 +1000

    Performed exercises on lists_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -9,7 +9,7 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers = False
 # "3" in numbers = False
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
-numbers[0] = "ten"
-numbers[-1] = 1
-print(numbers[2:])
-print(9 in numbers)
+numbers[0] = "ten"  # 1
+numbers[-1] = 1  # 2
+print(numbers[2:])  # 3
+print(9 in numbers)  # 4
